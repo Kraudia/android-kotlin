10b3a99ca4f3a10bd4ee4a9615fb46ba4bf74aba
Kraudia <klaudienne@gmail.com>
2017-11-13 20:47:33 +0100

    compile Anko Commons and SQLite

--- app/src/main/java/com/example/student/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/student/myapplication/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.student.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        new_password_textview.setVisibility(View.GONE)
+        new_password_edittext.setVisibility(View.GONE)
+        new_password_button.setVisibility(View.GONE)
+
+        see_message_button.setOnClickListener {
+            showMessage()
+        }
+    }
+
+    private fun showMessage() {
+        alert(R.string.fail_password_message, R.string.fail_password_title) {
+            positiveButton(R.string.close) { }
+        }.show()
     }
 }
